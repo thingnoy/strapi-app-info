8bbd8934abd9509711dee2a6f2dca4b0977c3789
supasai(boy) <boyskypart@gmail.com>
2024-06-19 13:46:45 +0700

    feat: handle case file not found

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -9,25 +9,43 @@ import { SettingsPageTitle } from '@strapi/helper-plugin';
 import { HeaderLayout, Layout, ContentLayout } from '@strapi/design-system/Layout';
 import { EmptyStateLayout } from '@strapi/design-system/EmptyStateLayout';
 import { Main } from '@strapi/design-system/Main';
-import packageJson from '../../../../../../../package.json';
-import appInfo from '../../../../../../../app-info.json';
+import { Box, Typography } from '@strapi/design-system';
+
+let packageJson = {};
+let appInfo = {};
+
+try {
+  packageJson = require('../../../../../../../package.json');
+} catch (error) {
+  console.error('package.json not found', error);
+}
+
+try {
+  appInfo = require('../../../../../../../app-info.json');
+} catch (error) {
+  console.error('app-info.json not found', error);
+}
 
 const HomePage = () => {
   return (
     <Layout>
-      <SettingsPageTitle name="App Information" />
+      <SettingsPageTitle name="App Info" />
       <Main>
-        <HeaderLayout title="App Information" subtitle="" />
+        <HeaderLayout title="App Info" subtitle="" />
       </Main>
       <ContentLayout>
         <EmptyStateLayout icon={<div />} content={`App version ${packageJson.version || '---'}`} />
         <Box padding={4} background="neutral100" hasRadius>
-          {Object.keys(appInfo).map((key) => (
-            <Box key={key} paddingBottom={2}>
-              <Typography variant="beta">{key}:</Typography>
-              <Typography>{appInfo[key]}</Typography>
-            </Box>
-          ))}
+          {Object.keys(appInfo).length > 0 ? (
+            Object.keys(appInfo).map((key) => (
+              <Box key={key} paddingBottom={2}>
+                <Typography variant="beta">{key}:</Typography>
+                <Typography>{appInfo[key]}</Typography>
+              </Box>
+            ))
+          ) : (
+            <Typography>No app information available</Typography>
+          )}
         </Box>
       </ContentLayout>
     </Layout>
